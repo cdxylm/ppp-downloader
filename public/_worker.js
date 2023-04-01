65fd59bf283404608bd6719c4a7768a0f0ddead2
aguo-mac <i@aguo.me>
2023-04-01 17:41:54 +0800

    solve CORS

--- public/_worker.js
+++ public/_worker.js
@@ -3,6 +3,60 @@ import * as Realm from 'realm-web';
 
 export default {
     async fetch(request, env) {
+        const corsHeaders = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+            "Access-Control-Max-Age": "86400",
+        };
+
+        async function handleRequest(request,dest) {
+            const url = new URL(request.url);
+
+            // Rewrite request to point to API URL. This also makes the request mutable
+            // ,so you can add the correct Origin header to make the API server think
+            // that this request is not cross-site.
+            request = new Request(dest, request);
+            request.headers.set("Origin", new URL(dest).origin);
+            let response = await fetch(request);
+            // Recreate the response so you can modify the headers
+
+            response = new Response(response.body, response);
+            // Set CORS headers
+
+            response.headers.set("Access-Control-Allow-Origin", url.origin);
+
+            // Append to/Add Vary header so browser will cache response correctly
+            response.headers.append("Vary", "Origin");
+
+            return response;
+        }
+
+        async function handleOptions(request) {
+            if (
+                request.headers.get("Origin") !== null &&
+                request.headers.get("Access-Control-Request-Method") !== null &&
+                request.headers.get("Access-Control-Request-Headers") !== null
+            ) {
+                // Handle CORS preflight requests.
+                return new Response(null, {
+                    headers: {
+                        ...corsHeaders,
+                        "Access-Control-Allow-Headers": request.headers.get(
+                            "Access-Control-Request-Headers"
+                        ),
+                    },
+                });
+            } else {
+                // Handle standard OPTIONS request.
+                return new Response(null, {
+                    headers: {
+                        Allow: "GET, HEAD, POST, OPTIONS",
+                    },
+                });
+            }
+        }
+
+
         let requestUrl = new URL(request.url)
         if (requestUrl.pathname.startsWith('/api/search')) {
             let name = requestUrl.searchParams.get("query")
@@ -103,10 +157,28 @@ export default {
         }
         if (requestUrl.pathname.startsWith('/api/files')) {
             const base = "https://www.cpppc.org:8082";
-            const statusCode = 301;
             const newPath = requestUrl.pathname.replace("/api/files", "/api/pub/project")
             const destinationURL = new URL(newPath, base)
-            return Response.redirect(destinationURL, statusCode);
+            // const statusCode = 301;
+            // return Response.redirect(destinationURL, statusCode);
+
+            if (request.method === "OPTIONS") {
+                // Handle CORS preflight requests
+                return handleOptions(request);
+            } else if (
+                request.method === "GET" ||
+                request.method === "HEAD" ||
+                request.method === "POST"
+            ) {
+                // Handle requests to the API server
+                return handleRequest(request,destinationURL);
+            } else {
+                return new Response(null, {
+                    status: 405,
+                    statusText: "Method Not Allowed",
+                });
+            }
+
         }
         return env.ASSETS.fetch(request);
     },
